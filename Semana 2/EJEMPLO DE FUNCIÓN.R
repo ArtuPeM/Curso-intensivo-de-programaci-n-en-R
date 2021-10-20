2dc753d0125df083fc385de0deeb4cb302845e8d
Arturo Pérez <arturodiciembre@gmail.com>
2021-10-20 18:55:16 -0500

    Semana 2

--- Semana 2/EJEMPLO DE FUNCIÓN.R
+++ Semana 2/EJEMPLO DE FUNCIÓN.R
@@ -1,3 +1,4 @@
+## 1 Medidas de tendencia central para 2 columnas de una base de datos.
 med.ten.cen <- function(x, y, data, nombres){
      data1 <- na.omit(x)
      data2 <- na.omit(y)
@@ -10,7 +11,18 @@ med.ten.cen <- function(x, y, data, nombres){
      tabulares
 }
 
-
-dat <-  med.ten.cen(airquality$Ozone, airquality$Solar.R, airquality, c("Ozone", "Solar.R"))
+med.ten.cen(airquality[, "Ozone"], airquality$Solar.R, airquality, c("Ozone", "Solar.R"))
 dat
 
+
+## 2 comparación entre grupos de una tabla.
+
+comp.2.grupos <- function(grupo1, grupo2){
+        grupo1 <- na.omit(grupo1)
+        grupo2 <- na.omit(grupo2)
+        error.est <- sqrt(var(grupo1)/length(grupo1) + var(grupo2)/length(grupo2))
+        valort <- (mean(grupo1)-mean(grupo2))/error.est
+        print(valort)
+}
+
+comp.2.grupos(DNase$conc, DNase$density)
